2733b5f6a1e0b24ef1e2e89d02185cbf5c239344
Juma <gerald.juma@student.moringaschool.com>
2025-03-16 20:41:00 +0300

    created and selected ramen elements for dynamic content updates

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+
+    const ramenImages = document.querySelectorAll('#ramen img');
+    const ramenName = document.querySelector('.name');
+    const ramenRestaurant = document.querySelector('.restaurant');
+    const ratingDisplay = document.querySelector('#rating-display');
+    const commentDisplay = document.querySelector('#comment-display');
+    const newRamenForm = document.querySelector('#new-ramen');
+
+
+});
